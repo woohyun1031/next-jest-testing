93e55ba06613f5e474426e68ac1afd156771d0a3
KimWooHyun <ktkwhms3@gmail.com>
2024-03-11 23:30:46 +0900

    Docs: README.md 추가

--- README.md
+++ README.md
@@ -1,36 +1,30 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Next, Jest testing
 
-## Getting Started
+## Purpose
 
-First, run the development server:
+- Next.js와 Jest를 사용하여 다양한 조건속 testing 환경 구축하기
+
+---
+
+## 개발 환경
+
+- **코어**: [Next.js App router v14.1.2](https://nextjs.org/), [TypeScript v5](https://www.typescriptlang.org/)
+
+- **스타일링**: [Tailwind CSS v3.3.0](https://tailwindcss.com)
+
+- **테스트**: [Jest](https://jestjs.io/)
+
+
+
+---
+
+## 로컬 실행
+
+해당 어플리케이션은 `Node.js v18.17` 이상을 요구하고 있습니다.  
 
 ```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+git clone https://github.com/woohyun1031/next-jest-testing.git
+cd my-app
+yarn install
+yarn run dev
 ```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- app/components/edu/body/CourseCard.tsx
+++ app/components/edu/body/CourseCard.tsx
@@ -7,8 +7,7 @@ import CardIconText from './CardIconText';
 import CardLabel from './CardLabel';
 import CardTitle from './CardTitle';
 
-export default function CourseCard(props: IOrgCourse) {
-  const freeType = props.enroll_type ? '구독' : props.is_free ? '무료' : '유료';
+export default function CourseCard(props: IOrgCourse) {  
   return (
     <Link
       href='/'
@@ -17,41 +16,10 @@ export default function CourseCard(props: IOrgCourse) {
     >
       <div className="relative h-card w-full rounded-lg bg-white">
         <div className="px-6 pt-7">
-          <CardLabel label={freeType ?? ''} />
+          <CardLabel label='미설정' />
           <CardTitle label={props.title} />
-          <CardDescription label={props.short_description} />
-          <div className="flex items-start justify-between">
-            <div className="flex flex-col">
-              <CardIconText
-                id={1}
-                src="/images/difficulty.svg"
-                label="난이도"
-                value="미설정"
-              />
-              <CardIconText
-                src="/images/class.svg"
-                label="수업"
-                value="온라인"
-                id={2}
-              />
-              <CardIconText
-                src="/images/period.svg"
-                label="기간"
-                value="무제한"
-                id={3}
-              />
-            </div>            
-          </div>
-        </div>
-        <div className="absolute bottom-0 w-full px-6 pb-7 pt-0">
-          <div className="w-full border-t border-solid border-gray-200 pt-4">
-            <span
-              className={`inline-block text-base font-bold leading-normal ${freeType === '구독' ? 'text-brand' : freeType === '무료' ? 'text-green-500' : 'text-gray-900'}`}
-            >
-              {freeType ?? ''}
-            </span>
-          </div>
-        </div>
+          <CardDescription label={props.short_description} />          
+        </div>        
       </div>
     </Link>
   );
